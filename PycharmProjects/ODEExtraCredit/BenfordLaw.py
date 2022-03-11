8ab08fcf91e22b6d0090ca493450ec8da829fe81
Joshua Shou <joshua2019175@concordiashanghai.org>
2022-03-11 17:03:19 -0600

    Add Benford

---
+++ PycharmProjects/ODEExtraCredit/BenfordLaw.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def Benfords(n, exponent):
+    bins = [0] * 10  # Bin array holding counts for each digit
+    num = 1
+    for i in range(n):
+        num *= exponent
+        digit = int(str(num)[0])
+        bins[digit] += 1
+    return bins
+
+
+if __name__ == '__main__':
+    benford_2 = Benfords(1000, 2)
+    plt.plot(benford_2)
+    plt.title("First digit for 2^n")
+    plt.xlabel("Digit")
+    plt.ylabel("Frequency")
+    axis = plt.subplot()
+    axis.set_xlim(1, 9)
+    plt.show()
+
+    benford_3 = Benfords(1000, 3)
+    plt.plot(benford_3)
+    plt.title("First digit for 3^n")
+    plt.xlabel("Digit")
+    plt.ylabel("Frequency")
+    axis = plt.subplot()
+    axis.set_xlim(1, 9)
+    plt.show()
+
+    benford_13 = Benfords(1000, 13)
+    plt.title("First digit for 13^n")
+    plt.xlabel("Digit")
+    plt.ylabel("Frequency")
+    plt.plot(benford_13)
+    axis = plt.subplot()
+    axis.set_xlim(1, 9)
+    plt.show()
+
+    benford_19 = Benfords(1000, 19)
+    plt.plot(benford_19)
+    plt.title("First digit for 19^n")
+    plt.xlabel("Digit")
+    plt.ylabel("Frequency")
+    axis = plt.subplot()
+    axis.set_xlim(1, 9)
+    plt.show()
